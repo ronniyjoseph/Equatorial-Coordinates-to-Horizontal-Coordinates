889f3afb175483662e4fde3d1a0db9a9f76c2ea0
ronniyjoseph <ronniyjoseph@gmail.com>
2019-07-12 09:55:15 +0800

    Made things more readable

--- Conversion.py
+++ Conversion.py
@@ -2,29 +2,53 @@ import numpy
 from matplotlib import pyplot
 
 def from_celestial_to_horizontal(HA, dec, L):
+    #Derive equations by writing down the sides of a triangle on a sphere, and the angles.
+    #Use the cosine rule to relate Elevation/Altitude to the sides
+    #Then use the cosine rule to relate Azimuthal angle to the sides
+    #Then use the sine rule to relate Azimuthal angle to the sides
+    #Divide second and third to get the tangent, i.e. to make Azimuth independent of Elevation.
+    #Now you have to solve the issue with the domain of the arctangent(Azimuth)
+    #Use the 2 argument tanget: https://en.wikipedia.org/wiki/Atan2
+
+
     E = numpy.arcsin(numpy.sin(dec)*numpy.sin(L) + numpy.cos(dec)*numpy.cos(HA)*numpy.cos(L))
-    A = numpy.arcsin(numpy.cos(dec)*numpy.sin(HA)/numpy.cos(E))
-    #A = numpy.arccos((numpy.sin(E)*numpy.sin(L) - numpy.sin(dec)/(numpy.cos(E)*numpy.cos(L))))
-    return E, A
+    #Use standard arctangent
+    A1 = numpy.arctan(numpy.sin(HA)/(numpy.cos(HA)*numpy.sin(L) - numpy.tan(dec)*numpy.cos(L)))
+    #use 2 argumented arctangent to account for the quadrants
+    A2 = numpy.arctan2(numpy.sin(HA), numpy.cos(HA)*numpy.sin(L) - numpy.tan(dec)*numpy.cos(L))
+
+    return E, A1, A2
 
 latitude = numpy.deg2rad(-26.5)
-source_dec = numpy.deg2rad(-80)
-hour_angles = numpy.linspace(0, 24, 24)
+source_dec = numpy.deg2rad(-10)
+hour_angles = numpy.linspace(-12, 12, 24)
 
-elevation, azimuth = from_celestial_to_horizontal(hour_angles/24*2*numpy.pi, source_dec, latitude)
+elevation, azimuth, azimuth2 = from_celestial_to_horizontal(hour_angles/24*2*numpy.pi, source_dec, latitude)
 
 print(elevation)
 print(numpy.deg2rad(azimuth))
 
-fig = pyplot.figure(figsize=(15,5))
+fig = pyplot.figure(figsize=(20,5))
 axes1= fig.add_subplot(131, polar = True)
 axes2= fig.add_subplot(132)
 axes3= fig.add_subplot(133)
 
-axes1.scatter(azimuth, numpy.pi/2 - elevation)
+axes1.scatter(azimuth, numpy.pi/2 - elevation, c='b', label = "arctan(x/y)", alpha = 0.4)
+axes1.scatter(azimuth2, numpy.pi/2 - elevation, c= 'r', label = "arctan2(x,y)",  alpha = 0.4)
 axes1.set_theta_zero_location('S')
 
-axes2.scatter(hour_angles, elevation)
-axes3.scatter(hour_angles, azimuth)
+axes2.scatter(hour_angles, elevation, c='k')
+axes3.scatter(hour_angles, azimuth, c='b', label = "arctan(x/y)",  alpha = 0.4)
+axes3.scatter(hour_angles, azimuth2, c='r', label = "arctan2(x,y)",  alpha = 0.4)
+
+axes2.set_xlabel("Hour Angle", fontsize = 15)
+axes3.set_xlabel("Hour Angle", fontsize = 15)
+
+axes2.set_ylabel("Elevation", fontsize = 15)
+axes3.set_ylabel("Azimuth", fontsize = 15)
+
+
+pyplot.legend()
+pyplot.savefig("comparing_arctan_arctan2.png")
 
 pyplot.show()
